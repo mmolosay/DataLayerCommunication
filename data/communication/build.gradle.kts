603cb61cf9681a2d1f91f278960e7b5866279dfe
mmolosay <mmolosays@gmail.com>
2023-01-31 23:40:27 +0300

    Add 'SerializationRequestEncoder' tests

--- data/communication/build.gradle.kts
+++ data/communication/build.gradle.kts
@@ -38,4 +38,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.1.0")
 }
