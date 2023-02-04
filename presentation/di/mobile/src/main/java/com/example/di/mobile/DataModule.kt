348df22a3c8fa64bfa56de47892e961caded187d
mmolosay <mmolosays@gmail.com>
2023-02-04 13:52:56 +0300

    Extract in-memory list of animals as a dependency to repository

--- presentation/di/mobile/src/main/java/com/example/di/mobile/DataModule.kt
+++ presentation/di/mobile/src/main/java/com/example/di/mobile/DataModule.kt
@@ -1,11 +1,12 @@
 package com.example.di.mobile
 
-import io.github.mmolosay.datalayercommunication.data.handheld.AnimalsRepositoryImpl
-import io.github.mmolosay.datalayercommunication.domain.repository.AnimalsRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.github.mmolosay.datalayercommunication.data.handheld.AnimalsRepositoryImpl
+import io.github.mmolosay.datalayercommunication.domain.model.Animal
+import io.github.mmolosay.datalayercommunication.domain.repository.AnimalsRepository
 import javax.inject.Singleton
 
 @Module
@@ -15,5 +16,56 @@ class DataModule {
     @Provides
     @Singleton
     fun provideAnimalsRepository(): AnimalsRepository =
-        AnimalsRepositoryImpl()
+        AnimalsRepositoryImpl(animals)
+
+    private val animals = listOf(
+        Animal(
+            id = 0L,
+            species = Animal.Species.Dog,
+            name = "Jake",
+            age = 3,
+        ),
+        Animal(
+            id = 1L,
+            species = Animal.Species.Cat,
+            name = "Cassy",
+            age = 1,
+        ),
+        Animal(
+            id = 2L,
+            species = Animal.Species.Rat,
+            name = "Scabbers",
+            age = 12,
+        ),
+        Animal(
+            id = 3L,
+            species = Animal.Species.Owl,
+            name = "Pig",
+            age = 1,
+        ),
+        Animal(
+            id = 4L,
+            species = Animal.Species.Cat,
+            name = "Sting",
+            age = 4,
+        ),
+        Animal(
+            id = 5L,
+            species = Animal.Species.Dog,
+            name = "Rocket",
+            age = 5,
+        ),
+        Animal(
+            id = 6L,
+            species = Animal.Species.Cat,
+            name = "Purr",
+            age = 0,
+        ),
+        Animal(
+            id = 7L,
+            species = Animal.Species.Owl,
+            name = "Merlin",
+            age = 2,
+        ),
+    )
 }
