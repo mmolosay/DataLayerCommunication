0d8bdc0bbd257cd6690fd42d40ece9ef3be1beec
mmolosay <mmolosays@gmail.com>
2023-02-19 19:59:58 +0100

    Add comments with link to an issue and its solution

--- utils/resource/src/main/kotlin/io/github/mmolosay/datalayercommunication/utils/resource/ResourceSerialializersModuleFactory.kt
+++ utils/resource/src/main/kotlin/io/github/mmolosay/datalayercommunication/utils/resource/ResourceSerialializersModuleFactory.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
 import kotlinx.serialization.modules.subclass
 
+// https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/polymorphism.md#polymorphism-and-generic-classes
 object ResourceSerialializersModuleFactory {
 
     fun make(): SerializersModule =
