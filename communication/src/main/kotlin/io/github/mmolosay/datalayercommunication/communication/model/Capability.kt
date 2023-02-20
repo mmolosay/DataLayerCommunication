02ddf6a4a73aff23d5f50c3c45198de233e6bac3
mmolosay <mmolosays@gmail.com>
2023-02-20 16:36:11 +0100

    Refine KDoc

--- communication/src/main/kotlin/io/github/mmolosay/datalayercommunication/communication/model/Capability.kt
+++ communication/src/main/kotlin/io/github/mmolosay/datalayercommunication/communication/model/Capability.kt
@@ -2,6 +2,7 @@ package io.github.mmolosay.datalayercommunication.communication.model
 
 /**
  * Identifier used to specify a capability of a specific node.
+ * Capabilities are used to distinguish between nodes and find an appropriate one(s).
  */
 @JvmInline
 value class Capability(val value: String)
