6b84036432b0ac3406de901d8696c7c252bdb198
mmolosay <mmolosays@gmail.com>
2023-02-23 12:12:59 +0100

    Provide dispatcher as a dependency

--- data/handheld/src/main/kotlin/io/github/mmolosay/datalayercommunication/data/handheld/AnimalsRepositoryImpl.kt
+++ data/handheld/src/main/kotlin/io/github/mmolosay/datalayercommunication/data/handheld/AnimalsRepositoryImpl.kt
@@ -5,24 +5,31 @@ import io.github.mmolosay.datalayercommunication.domain.model.Animals
 import io.github.mmolosay.datalayercommunication.domain.repository.AnimalsRepository
 import io.github.mmolosay.datalayercommunication.utils.resource.Resource
 import io.github.mmolosay.datalayercommunication.utils.resource.success
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class AnimalsRepositoryImpl(
-    animals: List<Animal>
+    animals: List<Animal>,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Default,
 ) : AnimalsRepository {
 
     private val animals = animals.toMutableList()
 
     override suspend fun getAllAnimals(): Resource<Animals> =
-        Animals(animals)
-            .let { Resource.success(it) }
+        withContext(dispatcher) {
+            Animals(animals)
+                .let { Resource.success(it) }
+        }
 
-    override suspend fun deleteAnimalById(id: Long): Resource<Animal?> {
-        if (animals.isEmpty()) return Resource.success(null)
-        return animals
-            .firstOrNull { it.id == id }
-            ?.also { animal ->
-                animals.remove(animal)
-            }
-            .let { Resource.success(it) }
-    }
+    override suspend fun deleteAnimalById(id: Long): Resource<Animal?> =
+        withContext(dispatcher) block@{
+            if (animals.isEmpty()) return@block Resource.success(null)
+            animals
+                .firstOrNull { it.id == id }
+                ?.also { animal ->
+                    animals.remove(animal)
+                }
+                .let { Resource.success(it) }
+        }
 }
--- data/wearable/src/main/kotlin/io/github/mmolosay/datalayercommunication/data/wearable/AnimalsRepositoryImpl.kt
+++ data/wearable/src/main/kotlin/io/github/mmolosay/datalayercommunication/data/wearable/AnimalsRepositoryImpl.kt
@@ -1,8 +1,8 @@
 package io.github.mmolosay.datalayercommunication.data.wearable
 
-import io.github.mmolosay.datalayercommunication.communication.failures.CommunicationFailures.ConnectionFailure
 import io.github.mmolosay.datalayercommunication.communication.NodeProvider
 import io.github.mmolosay.datalayercommunication.communication.client.CommunicationClient
+import io.github.mmolosay.datalayercommunication.communication.failures.CommunicationFailures.ConnectionFailure
 import io.github.mmolosay.datalayercommunication.communication.model.Path
 import io.github.mmolosay.datalayercommunication.communication.model.request.DeleteAnimalByIdRequest
 import io.github.mmolosay.datalayercommunication.communication.model.request.GetAllAnimalsRequest
@@ -15,35 +15,41 @@ import io.github.mmolosay.datalayercommunication.domain.model.Animals
 import io.github.mmolosay.datalayercommunication.domain.repository.AnimalsRepository
 import io.github.mmolosay.datalayercommunication.utils.resource.Resource
 import io.github.mmolosay.datalayercommunication.utils.resource.getOrElse
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class AnimalsRepositoryImpl(
     private val nodeProvider: NodeProvider,
     private val communicationClient: CommunicationClient,
     private val getAllAnimalsPath: Path,
     private val deleteAnimalByIdPath: Path,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : AnimalsRepository {
 
-    override suspend fun getAllAnimals(): Resource<Animals> {
-        val destination = nodeProvider
-            .singleConnectedHandheldNode()
-            .getOrElse { return ConnectionFailure }
-            .toDestination(getAllAnimalsPath)
-        val request = GetAllAnimalsRequest
-        return communicationClient
-            .request<GetAllAnimalsResponse>(destination, request)
-            .getOrElse { return it }
-            .resource
-    }
+    override suspend fun getAllAnimals(): Resource<Animals> =
+        withContext(dispatcher) block@{
+            val destination = nodeProvider
+                .singleConnectedHandheldNode()
+                .getOrElse { return@block ConnectionFailure }
+                .toDestination(getAllAnimalsPath)
+            val request = GetAllAnimalsRequest
+            communicationClient
+                .request<GetAllAnimalsResponse>(destination, request)
+                .getOrElse { return@block it }
+                .resource
+        }
 
-    override suspend fun deleteAnimalById(id: Long): Resource<Animal?> {
-        val destination = nodeProvider
-            .singleConnectedHandheldNode()
-            .getOrElse { return ConnectionFailure }
-            .toDestination(deleteAnimalByIdPath)
-        val request = DeleteAnimalByIdRequest(animalId = id)
-        return communicationClient
-            .request<DeleteAnimalByIdResponse>(destination, request)
-            .getOrElse { return it }
-            .resource
-    }
+    override suspend fun deleteAnimalById(id: Long): Resource<Animal?> =
+        withContext(dispatcher) block@{
+            val destination = nodeProvider
+                .singleConnectedHandheldNode()
+                .getOrElse { return@block ConnectionFailure }
+                .toDestination(deleteAnimalByIdPath)
+            val request = DeleteAnimalByIdRequest(animalId = id)
+            communicationClient
+                .request<DeleteAnimalByIdResponse>(destination, request)
+                .getOrElse { return@block it }
+                .resource
+        }
 }
