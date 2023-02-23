327e34926e2995260d6bb518b7ed9de4e052e8b3
mmolosay <mmolosays@gmail.com>
2023-02-23 20:49:07 +0100

    Mark 'UiState' as '@Stable'

--- app/wearable/src/main/java/io/github/mmolosay/datalayercommunication/models/UiState.kt
+++ app/wearable/src/main/java/io/github/mmolosay/datalayercommunication/models/UiState.kt
@@ -1,7 +1,9 @@
 package io.github.mmolosay.datalayercommunication.models
 
+import androidx.compose.runtime.Stable
 import io.github.mmolosay.datalayercommunication.domain.model.Animal
 
+@Stable
 data class UiState(
     val showConnectionFailure: Boolean,
     val elapsedTime: String,
