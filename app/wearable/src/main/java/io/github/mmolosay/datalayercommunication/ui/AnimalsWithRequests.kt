503789bcaeaecdc5cbea1465b6f022542ba2025f
mmolosay <mmolosays@gmail.com>
2023-02-24 09:52:46 +0100

    Change appearance of elapsed time text

--- app/wearable/src/main/java/io/github/mmolosay/datalayercommunication/ui/AnimalsWithRequests.kt
+++ app/wearable/src/main/java/io/github/mmolosay/datalayercommunication/ui/AnimalsWithRequests.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.Card
+import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.ScalingLazyColumn
 import androidx.wear.compose.material.ScalingLazyListScope
 import androidx.wear.compose.material.ScalingLazyListState
@@ -98,7 +100,11 @@ private fun ScalingLazyListScope.ElapsedTimeItem(
     elapsedTime: String,
 ) =
     item {
-        Text(text = "Request elapsed time: $elapsedTime")
+        Text(
+            text = "Request elapsed time: $elapsedTime",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.caption1,
+        )
         Spacer(modifier = Modifier.height(16.dp))
     }
 
