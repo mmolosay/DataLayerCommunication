eea6ff435ac33259ece5ec837016b5601eadc0b2
mmolosay <mmolosays@gmail.com>
2023-03-06 00:01:37 +0100

    Refine KDoc and add comment

--- communication/connection/src/main/java/io/github/mmolosay/datalayercommunication/communication/connection/ConnectionStateProvider.kt
+++ communication/connection/src/main/java/io/github/mmolosay/datalayercommunication/communication/connection/ConnectionStateProvider.kt
@@ -3,8 +3,9 @@ package io.github.mmolosay.datalayercommunication.communication.connection
 import kotlinx.coroutines.flow.Flow
 
 /**
- * Provides state of connection between current device and other one, specified in implementation.
+ * Provides state of connection between current device and other one, specified by implementation.
  */
+// It is placed in individual module, because it can be used by both handheld and wearable apps.
 interface ConnectionStateProvider {
 
     /**
