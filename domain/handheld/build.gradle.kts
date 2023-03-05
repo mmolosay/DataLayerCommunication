f56d1676a9c9b1ee69522eaa333829f86ab86470
mmolosay <mmolosays@gmail.com>
2023-03-05 23:55:13 +0100

    Make app-specific 'domain's depend upon ':domain:common'
    The idea is to reused models, common for both apps. However, since at the moment ':domain:handheld' and ':domain:wearable' are empty, this idea is to be tested

--- domain/handheld/build.gradle.kts
+++ domain/handheld/build.gradle.kts
@@ -10,6 +10,9 @@ java {
 }
 
 dependencies {
+    implementation(project(":domain:common"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("javax.inject:javax.inject:1")
 }
--- domain/wearable/build.gradle.kts
+++ domain/wearable/build.gradle.kts
@@ -10,7 +10,7 @@ java {
 }
 
 dependencies {
-    implementation(project(":communication"))
+    implementation(project(":domain:common"))
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
