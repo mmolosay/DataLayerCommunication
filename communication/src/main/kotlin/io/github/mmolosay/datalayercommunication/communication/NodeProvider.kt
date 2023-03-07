85cb23bf2644049375675a7e90029e29f9198964
mmolosay <mmolosays@gmail.com>
2023-03-07 22:46:27 +0100

    Refine KDoc

--- communication/src/main/kotlin/io/github/mmolosay/datalayercommunication/communication/NodeProvider.kt
+++ communication/src/main/kotlin/io/github/mmolosay/datalayercommunication/communication/NodeProvider.kt
@@ -9,11 +9,13 @@ interface NodeProvider {
 
     /**
      * Collects data about all reachable `handheld` nodes in current device network.
+     * This `suspend` function will switch coroutine dispatcher to an appropriate one.
      */
     suspend fun handheld(): Collection<Node>
 
     /**
      * Collects data about all reachable `wearable` nodes in current device network.
+     * This `suspend` function will switch coroutine dispatcher to an appropriate one.
      */
     suspend fun wearable(): Collection<Node>
 }
