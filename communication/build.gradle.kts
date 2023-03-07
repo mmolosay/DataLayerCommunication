270ab7f494bfb18ee3a805c0af563de4954c7e76
mmolosay <mmolosays@gmail.com>
2023-03-07 15:20:42 +0100

    Remove obsolete dependencies

--- communication/build.gradle.kts
+++ communication/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
-    id("org.jetbrains.kotlin.plugin.serialization")// TODO: remove
 }
 
 java {
@@ -11,10 +10,8 @@ java {
 
 dependencies {
     implementation(project(":communication:models"))
-    implementation(project(":domain:common:models")) // TODO: remove
     implementation(project(":utils:resource"))
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")// TODO: remove
     implementation("javax.inject:javax.inject:1") // TODO: remove separately
 }
