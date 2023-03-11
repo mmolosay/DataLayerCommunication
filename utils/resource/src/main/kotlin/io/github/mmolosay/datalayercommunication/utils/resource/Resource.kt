1a48e8afc351132d8647cd50abd0dc9263f66a7a
mmolosay <mmolosays@gmail.com>
2023-03-11 15:01:08 +0100

    Refine KDoc

--- utils/resource/src/main/kotlin/io/github/mmolosay/datalayercommunication/utils/resource/Resource.kt
+++ utils/resource/src/main/kotlin/io/github/mmolosay/datalayercommunication/utils/resource/Resource.kt
@@ -5,8 +5,15 @@ import io.github.mmolosay.datalayercommunication.utils.resource.Resource.Success
 import kotlinx.serialization.Serializable
 
 /**
- * `Resource` is a [Serializable] utility component, that can be either a successful result,
- * a.k.a [Success], or [Failure], which should be hoisted to presentation.
+ * `Resource` is a [Serializable] [monad](https://en.wikipedia.org/wiki/Monad_(functional_programming)),
+ * that encapsulates a result of some computation / obtaining.
+ * Actual instance can be either a [Success] or a [Failure].
+ *
+ * There's a little to no conceptual difference between a `Resource` and a [Result], beside that
+ * `Resource` is [Serializable].
+ * This is required in terms of this project, since it is being sent between devices (hence between different address spaces).
+ *
+ * @param T type of encapsulated value.
  */
 @Serializable
 sealed interface Resource<out T> {
